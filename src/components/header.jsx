a91361510cf90f066e0af66aca29108325a5f519
Hyeonsoo Joo <hyunsooting@gmail.com>
2021-11-09 22:56:17 +0900

    스크롤 시 헤더 감추기 기능 추가

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,18 +1,63 @@
-/* eslint-disable no-console */
 import React from 'react';
 
-export default function Header() {
-  return (
-    <header className="fixed top-0 left-0 flex flex-1 flex-row items-center justify-between w-full h-12 bg-dark">
-      <div className="leftDrawer p-4">
-        <span className="material-icons-round text-white">menu</span>
-      </div>
-      <div className="title p-4">
-        <h2 className="text-white">Hyeonsoo&apos;s Daily Log</h2>
-      </div>
-      <div className="rightIcon p-4">
-        <span className="material-icons-round text-white ">search</span>
-      </div>
-    </header>
-  );
+var prevLocation = 0;
+
+export default class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isHeaderHide: false,
+    };
+
+    this.handleHeaderScroll = this.handleHeaderScroll.bind(this);
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleHeaderScroll);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleHeaderScroll);
+  }
+
+  handleHeaderScroll(e) {
+    var currentY = window.scrollY;
+    prevLocation = prevLocation || 0;
+
+    // hide or display with a scroll direction
+    // console.log('current Location: ', currentY);
+    // console.log('prevLocation Location: ', prevLocation);
+
+    if (currentY > prevLocation && prevLocation > 0) {
+      this.setState({
+        isHeaderHide: true,
+      });
+    } else {
+      this.setState({
+        isHeaderHide: false,
+      });
+    }
+
+    prevLocation = currentY;
+  }
+
+  render() {
+    return (
+      <header
+        className={`transition-all transform ${
+          this.state.isHeaderHide ? '-translate-y-full' : ''
+        }  fixed top-0 left-0 flex flex-1 flex-row items-center justify-between w-full h-12 bg-dark `}
+      >
+        <div className="leftDrawer p-3 flex">
+          <span className="material-icons-round text-white">menu</span>
+        </div>
+        <div className="title p-3 flex">
+          <h2 className="text-white">Hyeonsoo&apos;s Daily Log</h2>
+        </div>
+        <div className="rightIcon p-3 flex">
+          <span className="material-icons-round text-white">search</span>
+        </div>
+      </header>
+    );
+  }
 }
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,8 +2,62 @@ import React from 'react';
 
 export default function Home() {
   return (
-    <main className="">
-      <p>this is home</p>
+    <main className="h-screen">
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore officiis ducimus,
+        molestias voluptatibus totam a eaque obcaecati mollitia? Autem quas repellat quasi magni,
+        iure odio temporibus consectetur nobis consequuntur nam.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore officiis ducimus,
+        molestias voluptatibus totam a eaque obcaecati mollitia? Autem quas repellat quasi magni,
+        iure odio temporibus consectetur nobis consequuntur nam.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore officiis ducimus,
+        molestias voluptatibus totam a eaque obcaecati mollitia? Autem quas repellat quasi magni,
+        iure odio temporibus consectetur nobis consequuntur nam.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore officiis ducimus,
+        molestias voluptatibus totam a eaque obcaecati mollitia? Autem quas repellat quasi magni,
+        iure odio temporibus consectetur nobis consequuntur nam.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore officiis ducimus,
+        molestias voluptatibus totam a eaque obcaecati mollitia? Autem quas repellat quasi magni,
+        iure odio temporibus consectetur nobis consequuntur nam.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore officiis ducimus,
+        molestias voluptatibus totam a eaque obcaecati mollitia? Autem quas repellat quasi magni,
+        iure odio temporibus consectetur nobis consequuntur nam.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore officiis ducimus,
+        molestias voluptatibus totam a eaque obcaecati mollitia? Autem quas repellat quasi magni,
+        iure odio temporibus consectetur nobis consequuntur nam.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore officiis ducimus,
+        molestias voluptatibus totam a eaque obcaecati mollitia? Autem quas repellat quasi magni,
+        iure odio temporibus consectetur nobis consequuntur nam.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore officiis ducimus,
+        molestias voluptatibus totam a eaque obcaecati mollitia? Autem quas repellat quasi magni,
+        iure odio temporibus consectetur nobis consequuntur nam.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore officiis ducimus,
+        molestias voluptatibus totam a eaque obcaecati mollitia? Autem quas repellat quasi magni,
+        iure odio temporibus consectetur nobis consequuntur nam.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore officiis ducimus,
+        molestias voluptatibus totam a eaque obcaecati mollitia? Autem quas repellat quasi magni,
+        iure odio temporibus consectetur nobis consequuntur nam.
+      </p>
     </main>
   );
 }
